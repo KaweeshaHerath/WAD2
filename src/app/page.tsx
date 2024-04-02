115f7ede7679bf09653e36caaf19c42b9d09a697
KaweeshaHerath <ar102224@fhss.sjp.ac.lk>
2024-04-02 10:56:27 +0530

    adding few technologies to about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
     <>
       <Layout>
         <Hero />
-        <About />
+        <About  />
         <Skills />
         <Experience />
         <FeaturedProjects />
