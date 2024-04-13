301ea66ce3df01a99fa6d07832cc45fc21fe82ae
KaweeshaHerath <ar102224@fhss.sjp.ac.lk>
2024-04-13 16:54:46 +0530

    Updating my project section

--- src/lib/content/featured-projects.ts
+++ src/lib/content/featured-projects.ts
@@ -14,16 +14,7 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
       img: 'https://user-images.githubusercontent.com/68834718/279476369-2f69466a-71db-4da0-9afd-04f8f0efb621.jpeg',
       tags: ['NextJs', 'TypeScript', ' CSS', 'React' ,'HTML'],
     },
-    {
-      id: getId(),
-      name: 'Calculator ',
-      description: 'Crafting Calculative Wonders: A Journey with NetBeans and Java. ',
-      tasks:
-        'In the vast expanse of the digital realm, your quest beckons: fashion a beacon of computational prowess using the venerable NetBeans software, your trusty companion on this odyssey through code.',
-      url: 'https://drive.google.com/drive/folders/15p0w6-8OWmRqEqJ7BD0-_k7QixiJ9ATY?usp=drive_link',
-      img: '/02.png',
-      tags: ['Open AI', 'OOP', 'Java', 'CSS' , 'HTML' ],
-    },
+    
   ],
 };
 
