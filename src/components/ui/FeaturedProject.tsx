a32adde81c4c9e08205d3b74673e618e13346b14
KaweeshaHerath <144052515+KaweeshaHerath@users.noreply.github.com>
2024-04-15 13:39:59 +0530

    Update FeaturedProject.tsx

--- src/components/ui/FeaturedProject.tsx
+++ src/components/ui/FeaturedProject.tsx
@@ -64,7 +64,7 @@ const FeaturedProject = ({
           className="absolute inset-0 z-50 block bg-transparent"
         />
         <a href='https://drive.google.com/file/d/1dVjvZE0FmXF0jsbfSf_ZI2d-piOQJvWb/view?usp=drive_link' className="absolute inset-0 z-50 block bg-transparent">
-          <img src ="05.png" alt="Image Alt Text" />
+          <Image src="/05.png" alt="Image Alt Text" layout="fill" />
         </a>
       </div>
       <div
