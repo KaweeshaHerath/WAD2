ac6a72f361feb5098dad4f7fbd9754fb7807072c
KaweeshaHerath <144052515+KaweeshaHerath@users.noreply.github.com>
2024-04-15 13:50:35 +0530

    Update Experience.tsx

--- src/containers/Experience.tsx
+++ src/containers/Experience.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+
+interface ExperienceProps {
+  // Type definitions here
+}
+
+const Experience: React.FC<ExperienceProps> = (props) => {
+  // Component logic
+  return <div>{/* JSX here */}</div>;
+};
+
+export default Experience;
 
